b48e6bdef32044276cd8dd23f981ba7bc85d9328
Zach Langford <langfzac@sas.upenn.edu>
2024-05-09 18:14:11 -0400

    Move make_test_inputs to utils file

--- src/KroneckerProductKernels.jl
+++ src/KroneckerProductKernels.jl
@@ -5,12 +5,13 @@ using LinearAlgebra, Random
 
 using Kronecker: ⊗
 
-import AbstractGPs: MeanFunction, FiniteGP
+import AbstractGPs: AbstractGP, MeanFunction, FiniteGP
 import Distributions, Random
 
-export KernelKroneckerProduct
+export KernelKroneckerProduct, KroneckerFiniteGP
 
 include("kernel.jl")
 include("gp.jl")
+include("utils.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,7 @@
 using KroneckerProductKernels, KernelFunctions, AbstractGPs, BenchmarkTools
 using Test, Random
 using KernelFunctions: TestUtils
-
-function make_test_inputs(Nx, Ny=nothing; seed=1234)
-    if Ny === nothing; Ny = Nx; end
-    rng = Random.MersenneTwister(seed)
-    xs = collect(1:1:Nx) .+ 0.1*randn(rng, Nx)
-    ys = collect(1:1:Ny) .+ 0.1*randn(rng, Ny)
-    return vcat([[x y] for x in xs for y in ys]...)
-end
+using KroneckerProductKernels: make_test_inputs
 
 @testset "KroneckerProductKernels.jl" begin
     include("test_kernel.jl")
