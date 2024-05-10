e61323bc6e64013a6a13e66aecda3ef29a2c08b4
Zach Langford <langfzac@sas.upenn.edu>
2024-05-10 16:27:08 -0400

    Add diagonal covariance tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,8 @@
-using KroneckerProductKernels, KernelFunctions, AbstractGPs, BenchmarkTools
-using Test, Random
+using AbstractGPs, KernelFunctions, KroneckerProductKernels, Kronecker
+using Test, BenchmarkTools, Random
 using KernelFunctions: TestUtils
 using KroneckerProductKernels: make_test_inputs
+using Kronecker: ⊗, Diagonal
 
 @testset "KroneckerProductKernels.jl" begin
     include("test_kernel.jl")
--- test/test_gp.jl
+++ test/test_gp.jl
@@ -2,33 +2,36 @@
     @testset "logpdf" begin
         # Test the logpdfs for a few combinations of kernels
         # Again, the computations for the tensor product kernel should match
-        X = make_test_inputs(100,50)
+        N, M = 100, 5
+        X = make_test_inputs(N,M)
         kernels = (ExponentialKernel(), SqExponentialKernel(), Matern52Kernel(), WhiteKernel())
         for k1 in kernels
             for k2 in kernels
                 k_kron = KernelKroneckerProduct(k1, k2)
                 k_tensor = KernelTensorProduct(k1, k2)
 
-                # Get FiniteGP for each
-                sigma = 0.1
-                fx_kron = GP(k_kron)(RowVecs(X), sigma)
-                fx_tensor = GP(k_tensor)(RowVecs(X), sigma)
+                # Test both uniform and diagonal covariance
+                covariances = (0.1, Diagonal(rand(N)) ⊗ Diagonal(rand(M)))
+                for covariance in covariances
+                    fx_kron = GP(k_kron)(RowVecs(X), covariance)
+                    fx_tensor = GP(k_tensor)(RowVecs(X), covariance)
                 
-                # draw from finite gp
-                Y = rand(fx_kron)
+                    # draw from finite gp
+                    Y = rand(fx_kron)
 
-                # Compare logpdfs 
-                @test logpdf(fx_kron, Y) ≈ logpdf(fx_tensor, Y)
+                    # Compare logpdfs 
+                    @test logpdf(fx_kron, Y) ≈ logpdf(fx_tensor, Y)
 
-                # Check type stability
-                @test @inferred(logpdf(fx_kron, Y)) isa Float64
+                    # Check type stability
+                    @test @inferred(logpdf(fx_kron, Y)) isa Float64
 
-                # Report benchmarks
-                #=@info k_kron.kernels
-                println("Kronecker: ")
-                @btime logpdf($fx_kron, $Y)
-                println("Tensor: ")
-                @btime logpdf($fx_tensor, $Y)=#
+                    # Report benchmarks
+                    #=@info k_kron.kernels covariance
+                    println("Kronecker: ")
+                    @btime logpdf($fx_kron, $Y)
+                    println("Tensor: ")
+                    @btime logpdf($fx_tensor, $Y)=#
+                end
             end
         end
     end
