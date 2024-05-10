a287d3deb8c7550435e6f85d8e01f9a90592595f
Zach Langford <langfzac@sas.upenn.edu>
2024-05-10 18:23:56 -0400

    add rand tests

--- test/test_gp.jl
+++ test/test_gp.jl
@@ -15,6 +15,42 @@
         end
     end
 
+    @testset "rand" begin
+        # Test that rand is functioning as expected
+        rng = Random.MersenneTwister(1234)
+        N, M = 10, 5
+        X = make_test_inputs(N, M)
+        kernel = KernelKroneckerProduct(SqExponentialKernel(), SqExponentialKernel())
+        
+        # Check for uniform and diagonal covariance
+        covariances = (0.1, Diagonal(rand(N)) ⊗ Diagonal(rand(M)))
+        for covariance in covariances
+            fx = GP(kernel)(RowVecs(X), covariance)
+            dist = convert(MvNormal, fx)
+
+            # Make sure rand methods work
+            @test length(rand(fx, 1)) == size(X,1)
+            @test size(rand(rng, fx, 10)) == (size(X,1), 10)
+            @test length(rand(fx)) == size(X,1)
+            @test size(rand(fx, 10)) == (size(X, 1), 10)
+
+            # Make sure returns real vector
+            @test rand(rng, fx) isa Vector{Float64}
+
+            # Check that samples converge to a MvNormal
+            ns = 1_000_000
+            f̂1 = rand(rng, fx, ns)
+            f̂2 = rand(rng, fx, ns)
+            for f̂ in (f̂1, f̂2)
+                @test maximum(abs.(mean(f̂1; dims=2) - mean(fx))) < 1e-2
+
+                res = (f̂ .- mean(fx))
+                Σp = res * res' ./ ns 
+                @test mean(abs.(Σp - cov(fx))) < 1e-2
+            end
+        end
+    end
+
     @testset "logpdf" begin
         # Test the logpdfs for a few combinations of kernels
         # Again, the computations for the tensor product kernel should match
